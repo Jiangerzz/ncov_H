e9ad507635bec6b61d14abc28c9d8854527502a1
Jiang <1347477532@qq.com>
2023-03-12 21:58:07 +0800

    nCoV后端初始化

---
+++ src/main/java/com/example/ncov_h/NcovHApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncov_h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcovHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcovHApplication.class, args);
+    }
+
+}
